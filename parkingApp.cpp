f7b4d9323a7cfad0a62f5e4e4655f8d3666bc0f7
chitiz panday <pandchit@isu.edu>
2023-07-04 18:34:30 -0600

    minor code changes

--- parkingApp.cpp
+++ parkingApp.cpp
@@ -1,72 +1,72 @@
-#include<iostream>
-
-   
+#include<iostream>  
 
 int main(){
    int t = 0;
    int b = 0;
    int c = 0;
    int amount = 0;
-   int total = 0;
-   int op;
+   int totalVehicle = 0;
+   int userInput;
    while(true){
      std::cout<<"Please press 1 for taxi parking \n";
      std::cout<<"Please press 2 for bus parking \n";
      std::cout<<"Please press 3 for car parking \n";
      std::cout<<"Please press 4 to check the record \n";
      std::cout<<"Please press 5 to delete the records \n";
-     std::cin>>op;
-
-     switch (op)
+     std::cin>>userInput;
+     switch (userInput)
      {
      case 1:
-      if(total<=50){
+      if(totalVehicle<=50){
          amount = amount + 200;
-         total = total + 1;
-         t = t + 1;
+         totalVehicle = totalVehicle + 1;
+         t += 1;
       }
       else{std::cout<<"Parking lot is full";}
       break;
 
       case 2:
-      if(total<=50){
+      if(totalVehicle<=50){
          amount = amount + 400;
-         total = total + 1;
+         totalVehicle = totalVehicle + 1;
          c = c + 1;
       }
       else{std::cout<<"Parking lot is full";}
       break;
 
        case 3:
-      if(total<=50){
+      if(totalVehicle<=50){
          amount = amount + 300;
-         total = total + 1;
+         totalVehicle = totalVehicle + 1;
          b = b + 1;
       }
       else{std::cout<<"Parking lot is full";}
       break;
 
       case 4:
+      std::cout<<"********************************" <<std::endl;
          std::cout<<"number of taxis parked: "<<t <<std::endl;
          std::cout<<"number of buses parked: "<<b <<std::endl;
          std::cout<<"number of car parked: "<<c <<std::endl;
          std::cout<<"Total amount is: $ "<<amount <<std::endl;
-         std::cout<<"Total number of vehicles parked: "<<total <<std::endl;
+         std::cout<<"Total number of vehicles parked: "<<totalVehicle <<std::endl;
+         std::cout<<"********************************" <<std::endl;
       break;
 
       case 5: 
          amount = 0;
-         total = 0;
+         totalVehicle = 0;
          b = 0;
          c = 0;
          t = 0;
       std::cout<<"********************************" <<std::endl;
-      std::cout<<"      Records Deleted";
+      std::cout<<"      Records Deleted \n";
       std::cout<<"********************************" <<std::endl;
       break;
      default:
      std::cout<<"Invalied Entry" <<std::endl;
      }
 }
+
    return 0;
 }
